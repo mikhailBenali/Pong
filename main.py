4da213e5185287d9ec72e4bd70c21746dfc4945d
Mikhaïl <94171094+mikhailBenali@users.noreply.github.com>
2022-02-20 22:23:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+screen_height = 800
+screen_width = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+first_pong_paddle = pygame.image.load("Paddle.png")
+first_paddle_x = 30
+
+second_pong_paddle = pygame.image.load("Paddle.png")
+second_paddle_x = 770
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
